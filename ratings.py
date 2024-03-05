4b4028372620e392e3d8b97741a74213eeb88ff1
Melissa McGrath <melissam640@gmail.com>
2024-03-04 16:55:29 -0800

    separated functions

--- ratings.py
+++ ratings.py
@@ -1,14 +1,14 @@
 """Restaurant rating lister."""
 
-
+    
+def sort_and_print(dictionary):
+    rating_score_items = sorted(dictionary.items())
+    
+    for key, value in rating_score_items:
+        print(f"{key} is rated at {value}")
+        
+    
 def restaurant_rating(text_file):
-    
-    def sort_and_print(dictionary):
-        rating_score_items = sorted(dictionary.items())
-    
-        for key, value in rating_score_items:
-            print(f"{key} is rated at {value}")
-    
     text_file = open(text_file)
     rating_score = {}
 
@@ -16,13 +16,15 @@ def restaurant_rating(text_file):
         words = line.rstrip().split(":") # [name of restaurant, rating]
         rating_score[words[0]] = words[1] # rating_score[name of resaturant (key)] = rating (value) => {name of restaurant: rating}
     
-    sort_and_print(rating_score)
+    return rating_score
+
     
+def user_restaurant_input(dictionary):
     user_restaurant = input("Please type a restaurant name")
     user_rating = input("Please add a score")
-    rating_score[user_restaurant] = user_rating
+    dictionary[user_restaurant] = user_rating
 
-    sort_and_print(rating_score)
+    return dictionary
     
 
         
