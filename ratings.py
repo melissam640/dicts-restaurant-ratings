bcfc8fd8f795df56b21b58fe9f6b5d3898e1b312
Melissa McGrath <melissam640@gmail.com>
2024-03-04 16:40:12 -0800

    Created function for sorting and printing restaurants

--- ratings.py
+++ ratings.py
@@ -3,18 +3,26 @@
 
 def restaurant_rating(text_file):
     
+    def sort_and_print(dictionary):
+        rating_score_items = sorted(dictionary.items())
+    
+        for key, value in rating_score_items:
+            print(f"{key} is rated at {value}")
+    
     text_file = open(text_file)
     rating_score = {}
 
     for line in text_file:
         words = line.rstrip().split(":") # [name of restaurant, rating]
         rating_score[words[0]] = words[1] # rating_score[name of resaturant (key)] = rating (value) => {name of restaurant: rating}
-    rating_score_items = sorted(rating_score.items()) # {[(key, value), (key, value), (key, value)]}
+    
+    sort_and_print(rating_score)
+    
+    user_restaurant = input("Please type a restaurant name")
+    user_rating = input("Please add a score")
+    rating_score[user_restaurant] = user_rating
 
-    for key, value in rating_score_items:
-        print(f"{key} is rated at {value}")
-input("Please type a restaurant name")
-input("Please add a score")
+    sort_and_print(rating_score)
     
 
         
