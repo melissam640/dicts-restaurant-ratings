940df58c9e62881ead11ae3931152ce4d0e55c09
Melissa McGrath <melissam640@gmail.com>
2024-03-04 16:18:59 -0800

    Completed first for loop

--- ratings.py
+++ ratings.py
@@ -1,4 +1,18 @@
 """Restaurant rating lister."""
 
 
-# put your code here
+def restaurant_rating(text_file):
+    
+    text_file = open(text_file)
+    rating_score = {}
+
+    for line in text_file:
+        words = line.rstrip().split(":") # [name of restaurant, rating]
+        rating_score[words[0]] = words[1] # rating_score[name of resaturant (key)] = rating (value) => {name of restaurant: rating}
+    rating_score_items = sorted(rating_score.items()) # {[(key, value), (key, value), (key, value)]}
+    print(rating_score_items)
+    # for key, vaue in rating_score.items():
+    #     print(restaurant_rating(f"{key} {vaue}"))
+    
+
+        
